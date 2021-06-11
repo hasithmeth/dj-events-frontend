9a771c47ee6e52ea6aa444f46fdac9de86eb6a0c
Hasith Methmal Jayasekara <hasith.jayasekara@rationaleye.com.au>
2021-06-12 00:21:00 +0530

    Upto register user

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -14,7 +14,23 @@ export const AuthProvider = ({ children }) => {
 
 	//Register user
 	const register = async (user) => {
-		console.log(user);
+		const res = await fetch(`${NEXT_URL}/api/register`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(user),
+		});
+
+		const data = await res.json();
+
+		if (res.ok) {
+			setUser(data.user);
+			router.push("/account/dashboard");
+		} else {
+			setError(data.message);
+			setError(null);
+		}
 	};
 
 	//Login user
@@ -31,7 +47,6 @@ export const AuthProvider = ({ children }) => {
 		});
 
 		const data = await res.json();
-		console.log(data);
 
 		if (res.ok) {
 			setUser(data.user);
@@ -44,7 +59,14 @@ export const AuthProvider = ({ children }) => {
 
 	//Logout user
 	const logout = async () => {
-		console.log("Logout");
+		const res = await fetch(`${NEXT_URL}/api/logout`, {
+			method: "POST",
+		});
+
+		if (res.ok) {
+			setUser(null);
+			router.push("/");
+		}
 	};
 
 	//Check if user is logged in
